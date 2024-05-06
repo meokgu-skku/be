8a73f666de4318667add0de667c10cdb3d35d8a6
sinkyoungdeok <tkagmd1@naver.com>
2024-05-07 00:30:12 +0900

    [KAN-34] 유저 - 이메일인증 메일 전송 API 개발 (패스워드 재설정 기능 추가)

--- src/main/kotlin/com/restaurant/be/user/domain/service/ValidateEmailService.kt
+++ src/main/kotlin/com/restaurant/be/user/domain/service/ValidateEmailService.kt
@@ -1,12 +1,14 @@
 package com.restaurant.be.user.domain.service
 
 import com.restaurant.be.common.exception.SendEmailException
+import com.restaurant.be.common.exception.SkkuEmailException
 import com.restaurant.be.common.redis.RedisRepository
 import com.restaurant.be.user.presentation.dto.SendEmailRequest
+import com.restaurant.be.user.presentation.dto.common.EmailSendType
 import com.restaurant.be.user.repository.EmailRepository
+import java.util.concurrent.TimeUnit
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.util.concurrent.TimeUnit
 
 @Service
 class ValidateEmailService(
@@ -15,31 +17,63 @@ class ValidateEmailService(
     private val redisRepository: RedisRepository
 ) {
     private val emailTemplate =
-        EmailRepository::class.java.classLoader.getResource("email-template.html")!!.readText()
+        EmailRepository::class.java.classLoader
+            .getResource("email-template.html")!!
+            .readText()
+    private val passwordTemplate =
+        EmailRepository::class.java.classLoader
+            .getResource("password-template.html")!!
+            .readText()
 
     fun sendValidateCode(request: SendEmailRequest) {
+        if (request.email.split("@")[1] != "g.skku.edu") {
+            throw SkkuEmailException()
+        }
+
         try {
             val code = emailRepository.generateRandomCode()
             val email = request.email
-            val message = software.amazon.awssdk.services.ses.model.SendEmailRequest
-                .builder()
-                .source(emailSource)
-                .destination {
-                    it.toAddresses(email)
-                }
-                .message {
-                    it.subject {
-                        it.data("먹꾸스꾸 회원가입 인증번호입니다.")
+            if (request.sendType == EmailSendType.EMAIL_VALIDATION) {
+                val message = software.amazon.awssdk.services.ses.model.SendEmailRequest
+                    .builder()
+                    .source(emailSource)
+                    .destination {
+                        it.toAddresses(email)
                     }
-                    it.body {
-                        it.html {
-                            it.data(emailTemplate.replace("AUTHENTICATION_CODE", code))
+                    .message {
+                        it.subject {
+                            it.data("먹꾸스꾸 회원가입 인증번호입니다.")
                         }
-                    }
-                }.build()
+                        it.body {
+                            it.html {
+                                it.data(emailTemplate.replace("AUTHENTICATION_CODE", code))
+                            }
+                        }
+                    }.build()
 
-            redisRepository.setValue("user:$email:emailCode", code, 3, TimeUnit.MINUTES)
-            emailRepository.sendEmail(message)
+                redisRepository.setValue("user:$email:emailCode", code, 3, TimeUnit.MINUTES)
+                emailRepository.sendEmail(message)
+            } else {
+                val message = software.amazon.awssdk.services.ses.model.SendEmailRequest
+                    .builder()
+                    .source(emailSource)
+                    .destination {
+                        it.toAddresses(email)
+                    }
+                    .message {
+                        it.subject {
+                            it.data("먹꾸스꾸 비밀번호 재설정 코드입니다.")
+                        }
+                        it.body {
+                            it.html {
+                                it.data(passwordTemplate.replace("AUTHENTICATION_CODE", code))
+                            }
+                        }
+                    }.build()
+
+                redisRepository.setValue("user:$email:passwordCode", code, 3, TimeUnit.MINUTES)
+                emailRepository.sendEmail(message)
+            }
         } catch (e: Exception) {
             throw SendEmailException()
         }
