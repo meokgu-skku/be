9c154bd29ae7415656fcc94038356b2afc4a21e9
sinkyoungdeok <tkagmd1@naver.com>
2024-05-06 23:45:37 +0900

    [KAN-34] 유저 - 이메일인증 메일 전송 API 개발

--- src/main/kotlin/com/restaurant/be/common/exception/ServerException.kt
+++ src/main/kotlin/com/restaurant/be/common/exception/ServerException.kt
@@ -32,11 +32,3 @@ data class DuplicateUserNickNameException(
 data class SendEmailException(
     override val message: String = "이메일 전송에 실패 했습니다."
 ) : ServerException(500, message)
-
-data class SkkuEmailException(
-    override val message: String = "성균관대 이메일이 아닙니다."
-) : ServerException(400, message)
-
-data class InvalidEmailCodeException(
-    override val message: String = "인증 코드가 일치 하지 않습니다."
-) : ServerException(400, message)
--- src/main/kotlin/com/restaurant/be/user/domain/service/ValidateEmailService.kt
+++ src/main/kotlin/com/restaurant/be/user/domain/service/ValidateEmailService.kt
@@ -1,17 +1,11 @@
 package com.restaurant.be.user.domain.service
 
-import com.restaurant.be.common.exception.InvalidEmailCodeException
 import com.restaurant.be.common.exception.SendEmailException
-import com.restaurant.be.common.exception.SkkuEmailException
 import com.restaurant.be.common.redis.RedisRepository
 import com.restaurant.be.user.presentation.dto.SendEmailRequest
-import com.restaurant.be.user.presentation.dto.ValidateEmailRequest
-import com.restaurant.be.user.presentation.dto.ValidateEmailResponse
-import com.restaurant.be.user.presentation.dto.common.EmailSendType
 import com.restaurant.be.user.repository.EmailRepository
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.util.*
 import java.util.concurrent.TimeUnit
 
 @Service
@@ -20,97 +14,34 @@ class ValidateEmailService(
     @Value("\${aws.sender-email}") private val emailSource: String,
     private val redisRepository: RedisRepository
 ) {
-    private val signUpTemplate =
-        EmailRepository::class.java.classLoader
-            .getResource("sign-up-template.html")!!
-            .readText()
-    private val resetPasswordTemplate =
-        EmailRepository::class.java.classLoader
-            .getResource("reset-password-template.html")!!
-            .readText()
+    private val emailTemplate =
+        EmailRepository::class.java.classLoader.getResource("email-template.html")!!.readText()
 
     fun sendValidateCode(request: SendEmailRequest) {
-        if (request.email.split("@")[1] != "g.skku.edu") {
-            throw SkkuEmailException()
-        }
-
         try {
             val code = emailRepository.generateRandomCode()
             val email = request.email
-            if (request.sendType == EmailSendType.SIGN_UP) {
-                val message = software.amazon.awssdk.services.ses.model.SendEmailRequest
-                    .builder()
-                    .source(emailSource)
-                    .destination {
-                        it.toAddresses(email)
+            val message = software.amazon.awssdk.services.ses.model.SendEmailRequest
+                .builder()
+                .source(emailSource)
+                .destination {
+                    it.toAddresses(email)
+                }
+                .message {
+                    it.subject {
+                        it.data("먹꾸스꾸 회원가입 인증번호입니다.")
                     }
-                    .message {
-                        it.subject {
-                            it.data("먹꾸스꾸 회원가입 인증번호입니다.")
+                    it.body {
+                        it.html {
+                            it.data(emailTemplate.replace("AUTHENTICATION_CODE", code))
                         }
-                        it.body {
-                            it.html {
-                                it.data(signUpTemplate.replace("AUTHENTICATION_CODE", code))
-                            }
-                        }
-                    }.build()
-
-                redisRepository.setValue(
-                    "user:$email:${request.sendType.name.lowercase(Locale.getDefault())}_code",
-                    code,
-                    3,
-                    TimeUnit.MINUTES
-                )
-                emailRepository.sendEmail(message)
-            } else {
-                val message = software.amazon.awssdk.services.ses.model.SendEmailRequest
-                    .builder()
-                    .source(emailSource)
-                    .destination {
-                        it.toAddresses(email)
                     }
-                    .message {
-                        it.subject {
-                            it.data("먹꾸스꾸 비밀번호 재설정 코드입니다.")
-                        }
-                        it.body {
-                            it.html {
-                                it.data(resetPasswordTemplate.replace("AUTHENTICATION_CODE", code))
-                            }
-                        }
-                    }.build()
+                }.build()
 
-                redisRepository.setValue(
-                    "user:$email:${request.sendType.name.lowercase(Locale.getDefault())}_code",
-                    code,
-                    3,
-                    TimeUnit.MINUTES
-                )
-                emailRepository.sendEmail(message)
-            }
+            redisRepository.setValue("user:$email:emailCode", code, 3, TimeUnit.MINUTES)
+            emailRepository.sendEmail(message)
         } catch (e: Exception) {
             throw SendEmailException()
         }
-    }
-
-    fun validateEmail(request: ValidateEmailRequest): ValidateEmailResponse {
-        if (!isValidCode(request.email, request.code, request.sendType)) {
-            throw InvalidEmailCodeException()
-        }
-
-        val token = (UUID.randomUUID().toString() + UUID.randomUUID().toString()).replace("-", "")
-        redisRepository.setValue(
-            "user:${request.email}:${request.sendType.name.lowercase(Locale.getDefault())}_token",
-            token,
-            3,
-            TimeUnit.MINUTES
-        )
-
-        return ValidateEmailResponse(token)
-    }
-
-    private fun isValidCode(email: String, code: String, sendType: EmailSendType): Boolean {
-        val key = "user:$email:${sendType.name.lowercase(Locale.getDefault())}_code"
-        return redisRepository.getValue(key) == code
     }
 }
