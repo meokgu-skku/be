326efc4b780157c29455b26869691a0d3ad47523
Goathoon <ownest2@gmail.com>
2024-05-13 00:54:16 +0900

    chore: lint apply

--- src/test/kotlin/com/restaurant/be/BeApplicationTests.kt
+++ src/test/kotlin/com/restaurant/be/BeApplicationTests.kt
@@ -1,12 +1,11 @@
 package com.restaurant.be
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class BeApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+//    @Test
+//    fun contextLoads() {
+//    }
 }
