7b12e1bd1a4facfc66e685407c2a9143e085777d
Goathoon <ownest2@gmail.com>
2024-05-12 16:31:41 +0900

    chore: lint apply again

--- src/main/kotlin/com/restaurant/be/review/domain/entity/Review.kt
+++ src/main/kotlin/com/restaurant/be/review/domain/entity/Review.kt
@@ -2,7 +2,16 @@ package com.restaurant.be.review.domain.entity
 
 import com.restaurant.be.common.entity.BaseEntity
 import com.restaurant.be.user.domain.entity.User
-import javax.persistence.*
+import javax.persistence.CascadeType
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
+import javax.persistence.Table
 
 @Entity
 @Table(name = "restaurant_reviews")
