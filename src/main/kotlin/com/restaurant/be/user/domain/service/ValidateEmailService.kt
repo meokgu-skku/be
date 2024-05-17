679bc12b3de7c061a79297dbc025d7a8a6dfd2f6
sinkyoungdeok <tkagmd1@naver.com>
2024-05-17 23:42:58 +0900

    [KAN-74] 성균관대 이메일 skku.edu도 될 수 있도록 변경

--- src/main/kotlin/com/restaurant/be/user/domain/service/ValidateEmailService.kt
+++ src/main/kotlin/com/restaurant/be/user/domain/service/ValidateEmailService.kt
@@ -30,7 +30,7 @@ class ValidateEmailService(
             .readText()
 
     fun sendValidateCode(request: SendEmailRequest) {
-        if (request.email.split("@")[1] != "g.skku.edu" ||
+        if (request.email.split("@")[1] != "g.skku.edu" &&
             request.email.split("@")[1] != "skku.edu"
         ) {
             throw SkkuEmailException()
