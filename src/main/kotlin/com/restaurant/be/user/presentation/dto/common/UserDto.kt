0d4b7831be8cd606afbfe13998e9f7bea9bcba09
Bob Sin <tkagmd1@naver.com>
2024-05-25 22:21:06 +0900

    [KAN-000] 유저 Dto에서 password 제거 (#59)

--- src/main/kotlin/com/restaurant/be/user/presentation/dto/common/UserDto.kt
+++ src/main/kotlin/com/restaurant/be/user/presentation/dto/common/UserDto.kt
@@ -9,9 +9,6 @@ data class UserDto(
     @ApiModelProperty(value = "이메일 아이디", example = "test@gmail.com", required = true)
     val email: String,
 
-    @ApiModelProperty(value = "비밀번호", example = "test12!@", required = true)
-    val password: String = "",
-
     @ApiModelProperty(value = "닉네임", example = "닉네임", required = true)
     val nickname: String,
 
