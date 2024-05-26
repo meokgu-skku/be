790c61e875c2bf6fdafde47ee67675fe4f08f567
sinkyoungdeok <tkagmd1@naver.com>
2024-05-27 00:37:44 +0900

    [KAN-60] 운영시간 사용안해서 제거

--- src/main/kotlin/com/restaurant/be/restaurant/presentation/controller/dto/common/RestaurantDto.kt
+++ src/main/kotlin/com/restaurant/be/restaurant/presentation/controller/dto/common/RestaurantDto.kt
@@ -19,10 +19,6 @@ data class RestaurantDto(
     val categories: List<String>,
     @Schema(description = "식당 대표 메뉴")
     val representativeMenu: MenuDto?,
-    @Schema(description = "식당 영업 시작 시간")
-    val operatingStartTime: String,
-    @Schema(description = "식당 영업 종료 시간")
-    val operatingEndTime: String,
     @Schema(description = "식당 대표 리뷰 내용")
     val representativeReviewContent: String?,
     @Schema(description = "식당 좋아요 여부(로그인한 유저)")
@@ -44,9 +40,7 @@ data class RestaurantDetailDto(
     @Schema(description = "식당 주소")
     val address: String,
     @Schema(description = "메뉴 정보")
-    val menus: List<MenuDto>,
-    @Schema(description = "영업 정보")
-    val operatingInfos: List<OperatingInfoDto>
+    val menus: List<MenuDto>
 )
 
 data class MenuDto(
@@ -60,13 +54,4 @@ data class MenuDto(
     val isRepresentative: Boolean,
     @Schema(description = "메뉴 이미지 URL")
     val imageUrl: String? = null
-)
-
-data class OperatingInfoDto(
-    @Schema(description = "영업 요일")
-    val day: String,
-    @Schema(description = "영업 시작 시간")
-    val startTime: String,
-    @Schema(description = "영업 종료 시간")
-    val endTime: String
 )
